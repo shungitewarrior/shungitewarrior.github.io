a41172736eda5bea850be1cf4f59126e62830662
shungitewarrior <92151791+shungitewarrior@users.noreply.github.com>
2023-06-07 12:36:38 +0200

    yep

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -3,7 +3,7 @@
     <h1>Press any of the following categories to display their items</h1>
     <hr />
     <ul>
-      {#each [["Users", "/users"], ["Address", "/addresses"], ["Banks", "/banks"], ["Appliances", "/appliances"], ["Beers", "/beers"], ["Blood Types", "/blood_types"], ["Credit Cards", "/credit_cards"]] as searchable}
+      {#each [["Users", "/users"]] as searchable}
         <li><a href={"/search" + searchable[1]}>{searchable[0]}</a></li>
       {/each}
     </ul>
